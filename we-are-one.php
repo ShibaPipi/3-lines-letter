c575bfa954d2eaf16d4f975fc256373853944f4c
ShibaPipi <906262260@qq.com>
2018-07-29 13:49:24 +0800

    A new One including the programming result

---
+++ we-are-one.php
@@ -0,0 +1,16 @@
+<?php
+	
+	$you = 'one';
+
+	$me = $you;
+
+	echo $you == $me ? "I am You. \nYou are Me. \nWe are {$me}." : '';
+
+	/**
+	 * 运行结果 ：
+	 * 
+	 * I am You.
+	 * You are Me.
+	 * We are one.
+	 *
+	 */
